50d865fafe476e8bea0dd852cc166a6955632a18
aslan <aslan.aslan@corp.zim.com>
2023-05-14 13:19:41 +0300

    section 3

---
+++ angular-course/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,19 @@
+//TODO: KEEP IN MINED: component in react its only java script function
+import './ExpenseItem.css'
+import './ExpenseDate'
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import React from "react";
+
+const ExpenseItem = (props) => {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}/>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>)
+}
+
+export default ExpenseItem;
